5b630728414f54dcab4451bd5aa6867e91cb93ff
Steve <eckods@hotmail.com>
2018-05-16 17:49:47 -0700

    Added Gif selection box and registration pw restriction

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,5 +1,7 @@
 import Controller from '@ember/controller';
 
+let gifs;
+
 export default Controller.extend({
   actions: {
     publishPost: function() {
@@ -12,22 +14,36 @@ export default Controller.extend({
       newPost.save();
       this.set('body', '');
     },
-    publishGIF: async function() {
+    publishGIF: function() {
       let queryText = this.get('body');
       let API_KEY = 'Xvt8ckTaWl0nl5WTPlq7cUcjSCHKZ4py';
-      let url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${queryText}&limit=25&offset=0&rating=G&lang=en`
+      let limit = '50';
+      let url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${queryText}&limit=${limit}&offset=0&rating=G&lang=en`;
       fetch(url)
         .then((response) => response.json())
         .then((res) => {
-          let newPost = this.store.createRecord('post', {
-            email: this.get("session.currentUser.email"),
-            body: `${res.data[0].images.original.url}`,
-            timestamp: new Date().getTime(),
-            image: true
-          });
-          newPost.save();
-          this.set('body','')
+          if (res.data.length <= 0 ) {
+            this.set('errorMessage', `GIF does not exist.`);
+            this.set('gifs', '');
+            return;
+          }
+          this.set('errorMessage', ``);
+          this.set('gifs', 'true');
+          gifs = res.data;
+          this.set('giffy', gifs)
         });
+    },
+    selectGIF: function (num) {
+      let newPost = this.store.createRecord('post', {
+        email: this.get("session.currentUser.email"),
+        body: `${num.images.original.url}`,
+        timestamp: new Date().getTime(),
+        image: true
+      });
+      this.set('gifs','');
+      this.set('giffy', '');
+      newPost.save();
+      this.set('body', '');
     }
   }
 });
--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -2,30 +2,39 @@ import Controller from '@ember/controller';
 import {inject as injectService} from '@ember/service';
 
 export default Controller.extend({
+    firebaseApp: Ember.inject.service(),
+    email: '',
+    password: '',
 
-    firebaseApp: injectService(),
+
     actions: {
         registerToDB() {
-
             const auth = this.get('firebaseApp').auth();
+
+
+            const email = this.get('email');
             const password = this.get('password');
-          auth.createUserWithEmailAndPassword(this.get('email'), this.get('password')).then((userResponse) => {
+            if (password.length < 6){
+              this.set('responseMessage', ``);
+              this.set('errorMessage', `Your password must be longer than 6 characters`);
+              return;
+            }
+
+            auth.createUserWithEmailAndPassword(email, password).
+                then((userResponse) => {
                     const newUser = this.store.createRecord('user', {
+
                         password: password,
                         id: userResponse.uid,
                         email: userResponse.email
-
                     });
                      newUser.save();
-                     this.set('responseMessage', `We have registered your email address: ${this.get('email')}`);
-                     this.set('email','')
-                     this.set('password','')
+                });
 
-                },(error) => {
-                  this.set('responseMessage',error);
-                })
-
-
+            this.set('responseMessage', `We have registered your email address:
+            ${this.get('email')}`);
+            this.set('errorMessage', ``);
+            this.set('emailAddress', '');
         }
     }
 });
